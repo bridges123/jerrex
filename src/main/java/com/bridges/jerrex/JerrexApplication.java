1ed918b71ca4c6ad00b5b8eef37ceb13db699e18
bridges123 <i.s.andreevmail.rut@gmail.com>
2023-02-21 14:28:42 +0300

    develop init

---
+++ src/main/java/com/bridges/jerrex/JerrexApplication.java
@@ -0,0 +1,13 @@
+package com.bridges.jerrex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JerrexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JerrexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridges/jerrex/JerrexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridges.jerrex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JerrexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
